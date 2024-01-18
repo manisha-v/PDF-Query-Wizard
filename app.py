fd3e948cf8c9d0a3d07976e3ef0c1c680e64022c
Manisha Varshney <94631536+manisha-v@users.noreply.github.com>
2024-01-18 11:27:49 +0530

    Update temperature value

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def get_conversational_chain():
     Answer:
     """
 
-    model = ChatGoogleGenerativeAI(google_api_key = key, model="gemini-pro", temperature=0.3)
+    model = ChatGoogleGenerativeAI(google_api_key = key, model="gemini-pro", temperature=0.7)
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
